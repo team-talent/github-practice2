2166dc2117e03e4a8e33764a1a9c63e7a31ff2c6
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-05-17 14:10:03 +0900

    [add]models and tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_17_031311) do
+ActiveRecord::Schema.define(version: 2020_05_17_050348) do
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "product_id"
+    t.integer "vol"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -18,10 +26,68 @@ ActiveRecord::Schema.define(version: 2020_05_17_031311) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
+    t.string "tel"
+    t.string "postal_code"
+    t.string "address"
+    t.boolean "customer_status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "destinations", force: :cascade do |t|
+    t.integer "customer_id"
+    t.string "name_tosend"
+    t.string "postcode_tosend"
+    t.string "address_tosend"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "details", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "order_id"
+    t.integer "product_vol"
+    t.integer "product_price"
+    t.integer "production_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "category_name"
+    t.boolean "genre_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id"
+    t.string "send_fee"
+    t.integer "total_price"
+    t.integer "pay"
+    t.string "name_tosend"
+    t.string "postcode_tosend"
+    t.string "address_tosend"
+    t.integer "order_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.integer "genre_id"
+    t.string "name"
+    t.integer "price"
+    t.string "image_id"
+    t.text "explain"
+    t.integer "sale_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "staffs", force: :cascade do |t|
